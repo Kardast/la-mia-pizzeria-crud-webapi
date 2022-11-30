d4d06d0112162268f0d426eb366875bddfcdd481
Kardast <dottorsf@gmail.com>
2022-11-30 17:11:44 +0100

    bonus

--- Models/Message.cs
+++ Models/Message.cs
@@ -8,17 +8,17 @@ namespace la_mia_pizzeria_static.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il campo email è obbligatorio")]
         [EmailAddress(ErrorMessage = "La mail deve avere un formato corretto")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il campo username è obbligatorio")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il campo testo è obbligatorio")]
         [StringLength(500, ErrorMessage = "Il messaggio non può essere più lunga di 500 caratteri")]
         [Column(TypeName = "text")]
         public string Text { get; set; }
